0ff2e2400f433868f98381fd4786f145aa2363ad
Mjd Dalloul <majed.dalloul79@gmail.com>
2021-11-28 15:52:36 +0200

    cache configuration

--- src/main/java/com/example/spring_tutorial/configuration/AppConstant.java
+++ src/main/java/com/example/spring_tutorial/configuration/AppConstant.java
@@ -3,6 +3,6 @@ package com.example.spring_tutorial.configuration;
 abstract public class  AppConstant {
     static public String share = "SHARE";
     static public String profitRate = "PROFIT_RATE";
-
+    static public final String appMainCacheName = "MAIN_CACHE";
     private AppConstant(){}
 }
