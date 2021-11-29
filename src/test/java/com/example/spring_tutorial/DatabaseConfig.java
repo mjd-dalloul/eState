1eba962f000c45bc6a44f88011dac61988e331db
Mjd Dalloul <majed.dalloul79@gmail.com>
2021-11-29 14:07:44 +0200

    Testing

---
+++ src/test/java/com/example/spring_tutorial/DatabaseConfig.java
@@ -0,0 +1,32 @@
+package com.example.spring_tutorial;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableJpaRepositories(basePackages = "com.example.spring_tutorial")
+@PropertySource("application_test.properties")
+@EnableTransactionManagement
+public class DatabaseConfig {
+    @Autowired
+    private Environment environment;
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(environment.getProperty("jdbc.driverClassName"));
+        dataSource.setUrl(environment.getProperty("jdbc.url"));
+        dataSource.setUsername(environment.getProperty("jdbc.user"));
+        dataSource.setPassword(environment.getProperty("jdbc.pass"));
+
+        return dataSource;
+    }
+}
---
+++ src/test/java/com/example/spring_tutorial/PropertyRepositoryTests.java
@@ -0,0 +1,82 @@
+package com.example.spring_tutorial;
+
+import com.example.spring_tutorial.configuration.AppConstant;
+import com.example.spring_tutorial.domain.entity.Constants;
+import com.example.spring_tutorial.domain.entity.Property;
+import com.example.spring_tutorial.repository.ConstantsRepository;
+import com.example.spring_tutorial.repository.PropertyRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+
+@DataJpaTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class PropertyRepositoryTests {
+
+    @Autowired
+    private PropertyRepository repository;
+    @Autowired
+    private ConstantsRepository constantsRepository;
+
+    @BeforeAll
+    public void mockData() {
+        constantsRepository.save(Constants.builder()
+                        .key(AppConstant.share)
+                        .value(7)
+                .build());
+        constantsRepository.save(Constants.builder()
+                        .key(AppConstant.profitRate)
+                        .value(5)
+                .build());
+        repository.save(Property.builder()
+                .description("test")
+                .price(213L)
+                .build());
+    }
+
+    @Test
+    public void addPropertyTest() {
+        Property property = Property.builder()
+                .description("test")
+                .shares(5)
+                .price(213L)
+                .build();
+        repository.save(property);
+        Assertions.assertThat(property.getId()).isGreaterThan(0);
+    }
+
+    @Test
+    public void getListOfPropertiesTest() {
+        final List<Property> property = repository.findAll();
+        Assertions.assertThat(property.size()).isGreaterThan(0);
+    }
+
+    @Test
+    public void testMockedData() {
+        final List<Constants> constants = constantsRepository.findAll();
+        Assertions.assertThat(constants.size()).isGreaterThan(0);
+    }
+
+    @Test
+    public void editProperty() {
+         Property p = repository.findAll().get(0);
+        p.setDescription("Just Edit that");
+        repository.save(p);
+        p = repository.findAll().get(0);
+        Assertions.assertThat(p.getDescription()).isEqualTo("Just Edit that");
+    }
+
+    @Test
+    public void deleteProperty() {
+        Property p = repository.findAll().get(0);
+        Long id = p.getId();
+        repository.delete(p);
+        Property deletedProperty =  repository.findById(id).orElse(null);
+        Assertions.assertThat(deletedProperty).isEqualTo(null);
+    }
+}
